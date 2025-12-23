5d46be2d4dc07ffc1d11b70fa32168763a857ecf
ASHNIW <srm651446@gmail.com>
2025-12-23 14:48:50 +0530

    practisef

---
+++ LAB_PRACTISE/OddEven.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class OddEven
+{
+public static void main(String[] args)
+{
+Scanner scanner = new Scanner(System.in);
+System.out.print("Enter integer number:");
+int number=scanner.nextInt();
+if (number%2==0)
+{
+System.out.println("The number "+number+" is even");
+}
+else
+{
+System.out.println("The number "+number+" is odd");
+}
+scanner.close();
+}
+}
+
