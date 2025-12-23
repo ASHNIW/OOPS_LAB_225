294e05167c45eb073be99b509f9722ab85992358
zeox <srm651446@gmail.com>
2025-12-23 15:01:07 +0530

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-// Save this file as HelloWorld.java
-public class HelloWorld {
-    public static void main(String[] args) {
-        // This line prints text to the terminal
-        System.out.println("Hello, World!");
-    }
-}
