dac8b78adc479cf0f63173d3667b899d8e372528
ASHNIW <srm651446@gmail.com>
2025-12-23 14:04:50 +0530

    practise

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+// Save this file as HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // This line prints text to the terminal
+        System.out.println("Hello, World!");
+    }
+}
